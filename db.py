520ec9017d2391058398374d757b39cfcf21035a
Pimenta017 <joaoduarte227@gmail.com>
2021-02-16 14:31:28 +0000

    Initial commit

---
+++ db.py
@@ -0,0 +1,8 @@
+class Database:
+
+    def __init__(self):
+
+        self.Host = 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com'
+        self.Database = 'dff8vulm5cjot2'
+        self.User = 'utzgdpchdtbstd'
+        self.Password = 'a82b53a3f7f69af19a95b027f24c8ed22cdc393e0651532e4f7fe1724489fb35'
